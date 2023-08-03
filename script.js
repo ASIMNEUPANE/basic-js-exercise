3f6819592cc5b8ebf4dcb0593bcb5b6a4bb24452
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-08-03 22:11:00 +0545

    solve new exercise

---
+++ script.js
@@ -0,0 +1,97 @@
+
+
+// // Q1 code
+
+// function sum(a,b){
+
+//     // let a = 4;
+
+//     // let b = 4;
+    
+//     if(a != b){
+//         console.log(a+b)
+//     }
+//     else(
+//         console.log(a+b*3)
+//     )
+
+// }
+
+// sum(4,4)
+
+
+// // Q2code
+
+// function check(a){
+
+// if (a/3 & a/7){
+
+// console.log("interger is a multiplication of 3 or 7")
+
+// }
+
+// else{
+//     console.log("integer is not a multiplication of 3 or 7")
+// }
+// }
+
+
+// check(70);
+
+
+// // Q4 code
+
+// function divmul(a,b){
+
+// console.log(a*b);
+// console.log(a/b);
+
+
+
+// }
+
+// divmul(10,2)
+
+// // Q5 code
+
+// let c = 60;
+// let f = 45
+
+// console.log(c*2.3333333)
+
+// console.log(f*0.160488889)
+
+
+// // Q6 code
+
+// console.log(Date())
+
+// Q5 solve
+
+
+let c = 60;
+let f=0;
+f=  (c * (9 / 5)) + 32;
+
+console.log("The value of the temperature in Fahrenheit is " + f);
+
+
+
+// Q6
+
+// const countVowels = (str)=>{
+
+//     const count = str.match(/[aeiou]/gi).length;
+
+//     return count;
+
+
+
+
+
+// }
+
+// let string = prompt (" enter string");
+// let result = countVowels(string);
+// console.log(result);
+
