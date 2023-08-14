278c0a210097986db2a09049c423688fc10d9df9
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-08-14 19:42:29 +0545

    solve one array problem

---
+++ js_exercise/array.js
@@ -0,0 +1,29 @@
+// const val = ['1', '2', '3'];
+// const add = val.reduce((a,b)=>{
+// return a+b,0
+// })
+
+// console.log(add());
+
+// Q1code
+
+const array = [1, 2, 3, 4, 5];
+
+const sum = array.reduce((total, currentValue) => total + currentValue, 0);
+
+console.log("Sum of array values:", sum);
+
+// Q2code
+
+const arr = [1,2,3,4,5,6];
+const find = arr.pop(0)
+console.log(find)
+
+// Write a program to find the maximum and minimum value of an array
+
+const arr1 = [1,2,3,4,5,6];
+
+
+
+
+
