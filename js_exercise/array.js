454c2fccd16444d031a6b2436a1a1762d4f668c6
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-08-21 17:16:57 +0545

    array new que solve

--- js_exercise/array.js
+++ js_exercise/array.js
@@ -1,27 +1,74 @@
-// const val = ['1', '2', '3'];
-// const add = val.reduce((a,b)=>{
-// return a+b,0
-// })
+// // const val = ['1', '2', '3'];
+// // const add = val.reduce((a,b)=>{
+// // return a+b,0
+// // })
 
-// console.log(add());
+// // console.log(add());
 
-// Q1code
+// // Q1code
 
-const array = [1, 2, 3, 4, 5];
+// const array = [1, 2, 3, 4, 5];
 
-const sum = array.reduce((total, currentValue) => total + currentValue, 0);
+// const sum = array.reduce((total, currentValue) => total + currentValue, 0);
 
-console.log("Sum of array values:", sum);
+// console.log("Sum of array values:", sum);
 
-// Q2code
+// // Q2code
 
-const arr = [1,2,3,4,5,6];
-const find = arr.pop(0)
-console.log(find)
+// const arr = [1,2,3,4,5,6];
+// const find = arr.pop(0)
+// console.log(find)
 
 // Write a program to find the maximum and minimum value of an array
 
-const arr1 = [1,2,3,4,5,6];
+// const array1 = [12, 45, 6, 89, 34, 67, 23];
+
+// // Using Math.max() and Math.min() with the spread operator
+// console.log(...array1);
+// console.log(array1);
+
+// const maxVal = Math.max(...array1);
+// const minVal = Math.min(...array1);
+
+// console.log("Maximum value:", maxVal);
+// console.log("Minimum value:", minVal);
+
+
+
+// Write a program to reverse an array of integer values.
+
+
+
+
+
+
+// const array = [12, 45, 6, 89, 34, 67, 23];
+// const reversedArray = array.reverse();
+
+// console.log("Original array:", array);
+// console.log("Reversed array:", reversedArray);
+
+
+// const array = [12, 45, 6, 89, 34, 67, 23];
+// const reversedArray = [...array].reverse();
+
+// console.log("Original array:", array);
+// console.log("Reversed array:", reversedArray);
+
+
+// 5.  Write a program to find the second largest element in an array.
+
+const numbers = [12, 45, 1, 78, 23, 65, 89, 32];
+
+// Sort the array in ascending order
+ numbers.sort(function(a, b) {
+    return a - b;
+});
+
+
+
+
+
 
 
 
