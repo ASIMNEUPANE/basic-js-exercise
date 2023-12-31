41925f6de35934e82201b367ba24e42f1e002a9d
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-31 23:40:38 +0545

    Fix bug in login functionality

---
+++ harkiratjs/week1/index.js
@@ -0,0 +1,29 @@
+// const firstName ='asim'
+// const lastName = 'neupane'
+// console.log(`Namaste ${firstName} ${lastName}`)
+
+// que2 WOP that greets a person base on  thie gender.(if else)
+
+// const name = 'asu';
+// const gender = prompt('enter your gender')
+
+// if(gender=== 'male'){
+//     alert("welcome male")
+// }else{
+//     alert('welcome female')
+// }
+
+// q3 Write a program that counts from 0 - 1000 and prints (for loop)
+
+// for (let index = 0; index < 101; index++) {
+//     console.log(index)
+    
+// }
+
+// 1. Write a program prints all the even numbers in an array
+
+// const arr = [1,2,3,4,5,6,7,8,9,10]
+// const hh = arr.filter((val)=>{
+//     return val %2 === 1;  
+// })
+// console.log(hh)
